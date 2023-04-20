e5cf43583efc03648604b39923756456328fb446
Brody14 <stefania.spinas@gmail.com>
2023-04-20 15:18:05 +0200

    FEAT seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,20 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+
+            $first_name = $faker->firstName();
+            $last_name = $faker->lastName();
+
+            $project = new Project();
+            $project->title = $faker->unique()->words(5, true);
+            $project->slug = Str::slug($project->title, '-');
+            $project->customer = $first_name . ' ' . $last_name;
+            $project->description = $faker->optional()->text();
+            $project->url = $faker->optional()->url();
+            $project->save();
+        }
     }
 }
